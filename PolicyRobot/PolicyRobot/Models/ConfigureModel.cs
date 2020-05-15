bda67538db765551e84bd3adaf9d31c34d6add10
Akbar Asghari <AkbarAsghari@outlook.com>
2020-05-15 12:29:14 +0430

    init

---
+++ PolicyRobot/PolicyRobot/Models/ConfigureModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolicyRobot.Models
+{
+    public class ConfigureModel : IConfigureModel
+    {
+        public string AccessToken { get; set; }
+    }
+
+    public interface IConfigureModel
+    {
+        public string AccessToken { get; set; }
+    }
+}
---
+++ PolicyRobot/PolicyRobot/Services/MessageService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using PolicyRobot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace PolicyRobot.Services
+{
+    public class MessageService
+    {
+        static ITelegramBotClient botClient;
+        public MessageService(IOptions<ConfigureModel> options)
+        {
+            botClient = new TelegramBotClient(options.Value.AccessToken);
+        }
+
+    }
+}
